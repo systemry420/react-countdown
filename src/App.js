51b95ce9f410e79b586ef98ea6d64dab0bc55caf
hsn <systemry420gmail.com>
2020-10-27 13:53:17 +0200

    add componentDidMount() to get new date

--- src/App.js
+++ src/App.js
@@ -14,23 +14,49 @@ class App extends Component {
     }
   }
   
+  componentDidMount() {
+    // get the new date every second
+    this.a = setInterval(()=>{
+      const newYearDate = '1 Jan 2021'
+      const diff = new Date(newYearDate) - new Date()
+      const total = Math.floor(diff / 1000)
+      const s = total % 60
+      const m = Math.floor(total / 60) % 60
+      const h = Math.floor(total / 3600) % 24
+      const d = Math.floor(total / 3600 / 24)
+      
+      // set the new date
+      this.setState({
+        data: [
+          { name: "Days", value: d },
+          { name: "Hours", value: h },
+          { name: "Minutes", value: m },
+          { name: "Seconds", value: s },
+        ]
+      })
+    }, 1000)
+
+  }
+  
   render() {
     return (
-      <div className="App">
-        {/* <Element data={this.state.data[0]}/> */}
-        {this.state.data.map((d, i)=>{
-          return <Element {...d} key={i} />
-        })}
+      <div>
+        <div className="App">
+          {this.state.data.map((d, i)=>{
+            return <Element setIntervalnt {...d} key={i} /> 
+          })}
+        </div>
       </div>
     );
   }
 }
 
-function Element({name, value}) {
+
+function Element(d) {
   return  (
     <div className="element">
-      <h2>{value}</h2>
-      <span>{name}</span>
+      <h1>{d.value}</h1>
+      <span>{d.name}</span>
     </div>
   )
 }
