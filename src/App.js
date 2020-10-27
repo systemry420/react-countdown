2355d4950135c3c099c916c9c59457a48dd7ee2f
hsn <systemry420gmail.com>
2020-10-27 10:23:19 +0200

    initialize state, and pass it as props to children

--- src/App.js
+++ src/App.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: [
+        { name: "Days", value: 0 },
+        { name: "Hours", value: 0 },
+        { name: "Minutes", value: 0 },
+        { name: "Seconds", value: 0 },
+      ]
+    }
+  }
+  
   render() {
     return (
       <div className="App">
-        hello
+        {/* <Element data={this.state.data[0]}/> */}
+        {this.state.data.map((d, i)=>{
+          return <Element {...d} key={i} />
+        })}
       </div>
     );
   }
 }
 
+function Element({name, value}) {
+  return  (
+    <div className="element">
+      <h2>{value}</h2>
+      <span>{name}</span>
+    </div>
+  )
+}
 export default App;
